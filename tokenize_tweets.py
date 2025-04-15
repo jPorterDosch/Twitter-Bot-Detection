a4e545675cdf6d30d94242c45046aeaecf9d8309
Josef Dosch <jdosch1@login1.cn.isaac.utk.edu>
2025-04-14 20:08:30 -0400

    almost done with preprocessing, switching to gpu environment to see if that speeds up tokenization

---
+++ tokenize_tweets.py
@@ -0,0 +1,63 @@
+import torch
+from transformers import AutoTokenizer, AutoModel
+import pandas as pd
+import numpy as np
+
+text_df = pd.read_parquet('data/revised_data.parquet')
+
+text_df.head()
+
+print(f"cuda is available: {torch.cuda.is_available()}")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+model = AutoModel.from_pretrained("bert-base-uncased").to(device)
+model.eval() # Turn off dropout layers for inference
+
+def embed_texts(texts, max_len=128, batch_size=256):
+    embs = []
+    for i in range(0, len(texts), batch_size):
+        batch = texts[i: i + batch_size]
+
+        # tokenize values
+        enc = tokenizer(
+            batch,
+            padding='longest',
+            truncation=True,
+            max_length=max_len,
+            return_tensors='pt', # return torch tensors
+        )
+
+        # Make sure any tensors returned by the tokenizer are on correct device to prevent mismatch
+        enc = {k: v.to(device) for k, v in enc.items() }
+
+        with torch.inference_mode():
+            out = model(**enc)
+        # Take out [CLS] token from last hidden state
+        cls_vectors = out.last_hidden_state[:, 0, :].cpu()
+        embs.append(cls_vectors)
+    return torch.cat(embs, dim=0).numpy()
+
+tweet_cols = [f'{i}' for i in range(1915)]
+
+# Average user tweet embeddings to get one embedding per user
+def embed_user_tweets(row):
+    tweets = []
+    for c in tweet_cols:
+        txt = row[c]
+        if pd.notna(txt) and isinstance(txt, str) and txt.lower() != 'none' and txt.strip():
+            tweets.append(txt)
+    if not tweets:
+        # 0 vector
+        return np.zeros(model.config.hidden_size, dtype=float)
+
+    tweet_embs = embed_texts(tweets)
+
+    # Average tweet values
+    return tweet_embs.mean(axis=0)
+
+user_vectors = text_df.apply(embed_user_tweets, axis = 1)
+user_vectors.head()
+
+user_vectors.to_parquet('/lustre/isaac/scratch/jdosch1/COSC325_Final')
