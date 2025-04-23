b2cbd99f58e769ff826e8daec69ea56155e7eb0e
Josef Dosch jdosch1@vols.utk.edu <jdosch1@com2044.eecs.utk.edu>
2025-04-22 20:33:43 -0400

    model trains, now optimizing for accuracy

--- deep_learning/nn_model.py
+++ deep_learning/nn_model.py
@@ -1,5 +1,18 @@
 import torch
 
+class SimpleMLP(torch.nn.Module):
+    def __init__(self, length):
+        super().__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Flatten(),
+            torch.nn.Linear(length, 32),
+            torch.nn.ReLU(),
+            torch.nn.Linear(32, 2)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+
 class ResNet_Block(torch.nn.Module):
     def __init__(self, in_channels, out_channels, stride=1):
         super(ResNet_Block, self).__init__()
@@ -10,21 +23,18 @@ class ResNet_Block(torch.nn.Module):
         self.bn1 = torch.nn.BatchNorm1d(num_features=out_channels)
         self.relu = torch.nn.ReLU()
 
-        self.conv2 = torch.nn.Conv1d(in_channels=out_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1)
-        self.bn2 = torch.nn.BatchNorm1d(num_features=out_channels)
+        self.conv2 = torch.nn.Conv1d(in_channels=out_channels, out_channels=in_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn2 = torch.nn.BatchNorm1d(num_features=in_channels)
 
-        self.conv3 = torch.nn.Conv1d(in_channels=out_channels, out_channels=in_channels, kernel_size=3, stride=stride, padding=1)
-        self.bn3 = torch.nn.BatchNorm1d(num_features=in_channels)
-
-        self.conv4 = torch.nn.Conv1d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=stride, padding=1)
-        self.bn4 = torch.nn.BatchNorm1d(num_features=in_channels)
+        self.conv3 = torch.nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1)
+        self.bn3 = torch.nn.BatchNorm1d(num_features=out_channels)
 
         # Downsample if an odd stride shape causes mismatch in shapes that would prevent skip connection addition
         self.downsample = None
         if stride != 1 or in_channels != out_channels: 
             self.downsample = torch.nn.Sequential(
-                torch.nn.Conv1d(in_channels, in_channels, kernel_size=1, stride=stride),
-                torch.nn.BatchNorm1d(in_channels)
+                torch.nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=stride),
+                torch.nn.BatchNorm1d(out_channels)
             )
     
     def forward(self, x):
@@ -57,7 +67,7 @@ class DNN(torch.nn.Module):
         super(DNN, self).__init__()
         self.num_classes = 2
         self.input_channels = 1
-        self.output_channels = 4
+        self.output_channels = 16
 
         # ResNet
         self.conv = torch.nn.Conv1d(in_channels=self.input_channels, out_channels=self.input_channels, kernel_size=3, stride=1, padding=1)
